23628731e3665c51c6a24dae2d426bfc878a876d
Matt Fawcett <mattfawcett@matt-fawcetts-imac.local>
2010-03-05 14:48:55 +0000

    construnct the TrafficSources just using a string

--- test/traffic_sources_test.rb
+++ test/traffic_sources_test.rb
@@ -2,11 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/test_helper')
 
 class TrafficSourcesTest < Test::Unit::TestCase
   context "TrafficSourcesTest" do
-    context "retrieve" do
-      should "return 2 traffic source objects if I have 2 strings in the cookie" do
-        @rack_env = EXAMPLE_RACK_ENV.dup
-        @rack_env["rack.session"][:traffic_sources] = "1|#{Time.now.to_i}|direct,1|#{Time.now.to_i}|cpc|Product One|google|MyCamp1"
-        assert_equal 2, TrafficSources.retrieve(@rack_env).length
+    context "retrieve" do      
+      should "allow the raw string to be used to build the array of TrafficSources" do
+        assert_equal 2, TrafficSources.retrieve("1|#{Time.now.to_i}|direct,1|#{Time.now.to_i}|cpc|Product One|google|MyCamp1").length
       end
     end
   end
--- traffic_sources.rb
+++ traffic_sources.rb
@@ -1,6 +1,8 @@
 class TrafficSources < Array
-  def self.retrieve(rack_env)
-    traffic_source_strings = rack_env["rack.session"][:traffic_sources].split(",")
+  
+  #The input can be either a comma separated string or a rack environment
+  def self.retrieve(string)    
+    traffic_source_strings = string.split(",")
     traffic_sources = TrafficSources.new
     traffic_source_strings.each do |string|
       traffic_sources << TrafficSource.initialize_from_string(string)
