fa47ee45eb8071ee06777f1949b06b6ebaf17140
Matt Fawcett <mattfawcett@matt-fawcetts-imac.local>
2010-03-05 14:54:46 +0000

    removed wrong comment

--- traffic_sources.rb
+++ traffic_sources.rb
@@ -1,6 +1,5 @@
 class TrafficSources < Array
   
-  #The input can be either a comma separated string or a rack environment
   def initialize(string)    
     traffic_source_strings = string.split(",")
     traffic_source_strings.each do |string|
